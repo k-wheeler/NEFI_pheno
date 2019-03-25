205734c601e52022efa7575d5d363b81d09ee95c
Kathryn Wheeler <kiwheel@bu.edu>
2019-03-25 12:24:50 -0400

    fixed error

--- PhenoForecast/R/load_NOAA_met.R
+++ PhenoForecast/R/load_NOAA_met.R
@@ -15,7 +15,7 @@ load_NOAA_met <- function(lat,long,years) {
                            longitude = long)
   nearby_stations <- meteo_nearby_stations(lat_lon_df = sites_data, station_data = station_data, year_min = 2018, year_max = 2018, radius = 30, var = "all")
   station <- as.character(nearby_stations[[1]][1,1])
-  data = meteo_tidy_ghcnd(stationid = station,var = c("TAVG","tmin","tmax"), date_min = "2019-01-01", date_max = (Sys.Date()-1))
+  dat = meteo_tidy_ghcnd(stationid = station,var = c("TAVG","tmin","tmax"), date_min = "2019-01-01", date_max = (Sys.Date()-1))
 
   NOAAavgs <- rowMeans(cbind(dat$tmax,dat$tmin))
   return(NOAAavgs)
