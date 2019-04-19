84368c02fe0c4a35a32385b0e2f9b8c54a9d3299
Kathryn Wheeler <kiwheel@bu.edu>
2019-04-19 14:23:47 -0400

    debugging

--- PhenoForecast/R/calSf.R
+++ PhenoForecast/R/calSf.R
@@ -4,12 +4,12 @@
 ##' @param dates The desired dates
 ##' @export
 calSf <- function(Tairs,dates) {
-  #print(dates)
+  print(dates)
   Rf <- calRf(Tairs[1])
   cumTotal <- Rf
   Sfs <- Rf
   for(i in 2:length(Tairs)){
-    #print(dates[i])
+    print(dates[i])
     if((lubridate::day(dates[i])==1)&&(lubridate::month(dates[i])==1)){ ##Reset every year
       cumTotal <- 0
     }
