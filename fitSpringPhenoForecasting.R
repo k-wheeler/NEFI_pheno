0dfe38736ea5721dc8daaa7eee4e7c73131292d8
kiwheel@bu.edu <kiwheel@scc-gb13.scc.bu.edu>
2019-04-24 12:07:18 -0400

    merging

--- fitSpringPhenoForecasting.R
+++ fitSpringPhenoForecasting.R
@@ -15,7 +15,7 @@ library("doParallel")
 endDate <- as.Date("2019-01-27")
 forecastLength <- 0
 
-n.cores <- 6
+#n.cores <- 6
 
 #register the cores.
 registerDoParallel(cores=n.cores)
@@ -24,9 +24,9 @@ iseq <- c(seq(2,6),8,9,11,seq(15,20))
 #iseq <- c(10)
 siteData <- read.csv("PhenologyForecastData/phenologyForecastSites.csv",header=TRUE)
 
-output <- 
-  foreach(i=iseq) %dopar% {
-#for(i in iseq){
+#output <- 
+#  foreach(i=iseq) %dopar% {
+for(i in iseq){
   if(as.character(siteData$PFT[i])=="DB"){
     siteName <- as.character(siteData[i,1])
     print(siteName)
