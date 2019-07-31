831cc8084efb6b1f67f444c8b90449bf549101e1
Kathryn Wheeler <kiwheel@bu.edu>
2019-07-31 18:08:47 -0400

    typo

--- PhenoForecast/R/plotRun.R
+++ PhenoForecast/R/plotRun.R
@@ -18,7 +18,7 @@ plotRun <- function(out.mat,forecastType,endDate){
   lastYearIndices <- seq(((dayNumber-lengthLastYear)+1),dayNumber,1)
   out.mat.lastYear <- out.mat[,lastYearIndices]
 
-  plotForecastOutput(siteName=siteName,URL=URL,forecastLength=0,out.mat=out.mat.lastYear,forecastType = forecastType,days=seq(1,lengthLastYear,1),xlim=c(0,550))
+  plotForecastOutput(siteName=siteName,URL=URL,forecastLength=0,out.mat=out.mat.lastYear,forecastType = forecastType,days=seq(1,lengthLastYear,1),xlim=c(0,550),endDate=endDate)
 
 
 }
