bef648311ba834662e0f7399afa645629a23f260
Kathryn Wheeler <kiwheel@bu.edu>
2019-07-31 18:04:26 -0400

    loop over all dates

--- springUncertaintyAnalysis.R
+++ springUncertaintyAnalysis.R
@@ -9,7 +9,8 @@ library(ecoforecastR)
 siteData <- read.csv("PhenologyForecastData/phenologyForecastSites.csv",header=TRUE)
 forecastDataFolder <- "PhenologyForecastData/ForecastOutputs/AllForecasts/"
 Nmc <- 1000 #Number of model runs
-i <- 1
+allDates <- seq(as.Date("2019-01-23"),as.Date("2019-06-06"),"day")
+
 
 ##General site-specific info
 siteName <- as.character(siteData$siteName[i])
@@ -20,186 +21,189 @@ long <- as.numeric(siteData$Long[i])
 station <- as.character(siteData$metStation[i])
 siteStartDate <- as.character(siteData$startDate[i])
 pdf(file=paste(siteName,"_PhenologyForecast_uncertaintyAnalysis.pdf",sep=""),height=6,width=10)
-
+par(mfrow=c(4,2))
 
 ##Date info 
-calEndDate <- as.Date("2019-01-23")
-forStartDate <- calEndDate + 1
-forEndDate <- (forStartDate+13)
-forDates <- seq(forStartDate,forEndDate,"day")
-plotDates <- as.numeric(format(forDates,"%j"))
-forecastLength <- 14
-
-############Random Walk
-RWfileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_randomWalk_outBurn.RData",sep="")
-load(RWfileName)
-out.mat.par <- data.frame(as.matrix(outBurnRW$params))
-dayNumber <- dim(as.matrix(outBurnRW$predict))[2]-14
-out.mat.RW <- as.matrix(outBurnRW$predict)
-out.mat.RW <- out.mat.RW[,1:dayNumber]
-ICs <- out.mat.RW[,dayNumber]
-
-##Deterministic prediction
-RW.det <- forecastRW(IC=mean(ICs),
-                    Q=0,
-                    n=1,
-                    NT=14)
-
-##Initial Condition Uncertainty
-rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
-RW.IC <- forecastRW(IC=ICs[rndNums],
-                    Q=0,
-                    n=Nmc,
-                    NT=14)
-RW.IC.ci <- apply(RW.IC,2,quantile,c(0.025,0.5,0.975))
-
-##Process Uncertainty
-Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
-RW.IPDE <- forecastRW(IC=ICs[rndNums],
-                      Q=Qmc,
+for(d in 1:length(allDates)){
+  
+  calEndDate <- allDates[d]
+  forStartDate <- calEndDate + 1
+  forEndDate <- (forStartDate+13)
+  forDates <- seq(forStartDate,forEndDate,"day")
+  plotDates <- as.numeric(format(forDates,"%j"))
+  forecastLength <- 14
+  
+  ############Random Walk
+  RWfileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_randomWalk_outBurn.RData",sep="")
+  load(RWfileName)
+  out.mat.par <- data.frame(as.matrix(outBurnRW$params))
+  dayNumber <- dim(as.matrix(outBurnRW$predict))[2]-14
+  out.mat.RW <- as.matrix(outBurnRW$predict)
+  out.mat.RW <- out.mat.RW[,1:dayNumber]
+  ICs <- out.mat.RW[,dayNumber]
+  
+  ##Deterministic prediction
+  RW.det <- forecastRW(IC=mean(ICs),
+                       Q=0,
+                       n=1,
+                       NT=14)
+  
+  ##Initial Condition Uncertainty
+  rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
+  RW.IC <- forecastRW(IC=ICs[rndNums],
+                      Q=0,
                       n=Nmc,
                       NT=14)
-RW.IPDE.ci <- apply(RW.IPDE,2,quantile,c(0.025,0.5,0.975))
-plotRun(out.mat = out.mat.RW,forecastType = "randomWalk")
-ecoforecastR::ciEnvelope(time2,RW.IPDE.ci[1,],RW.IPDE.ci[3,],col=adjustcolor("blue",0.8))
-ecoforecastR::ciEnvelope(time2,RW.IC.ci[1,],RW.IC.ci[3,],col=adjustcolor("gray",0.8))
-lines(time2,RW.det,col="purple",lwd=2)
-
-#################Basic Logistic
-logFileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_logistic_outBurn.RData",sep="")
-load(logFileName)
-out.mat.par <- data.frame(as.matrix(outBurnL$params))
-dayNumber <- dim(as.matrix(outBurnL$predict))[2]-14
-out.mat.L <- as.matrix(outBurnL$predict)
-out.mat.L <- out.mat.L[,1:dayNumber]
-ICs <- out.mat.L[,dayNumber]
-
-##Deterministic prediction
-L.det <- forecastLog(IC=mean(ICs),
-                     r=mean(out.mat.par$r),
-                     Q=0,
-                     n=1,
-                     NT=14)
-
-##Initial Condition Uncertainty
-rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
-L.I <- forecastLog(IC=ICs[rndNums],
-                    r=mean(out.mat.par$r),
-                    Q=0,
-                    n=Nmc,
-                    NT=14)
-L.I.ci <- apply(L.I,2,quantile,c(0.025,0.5,0.975))
-
-##Parameter Uncertainty <- Don't have for RW (will have for log and logCov)
-L.IP <- forecastLog(IC=ICs[rndNums],
-                    r=out.mat.par$r[rndNums],
-                    Q=0,
-                    n=Nmc,
-                    NT=14)
-L.IP.ci = apply(L.IP,2,quantile,c(0.025,0.5,0.975))
-
-##Process Uncertainty
-Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
-L.IPDE <- forecastLog(IC=ICs[rndNums],
-                    r=out.mat.par$r[rndNums],
-                    Q=Qmc,
-                    n=Nmc,
-                    NT=14)
-L.IPDE.ci <- apply(L.IPDE,2,quantile,c(0.025,0.5,0.975))
-
-plotRun(out.mat = out.mat.L,forecastType = "logistic")
-ecoforecastR::ciEnvelope(time2,L.IPDE.ci[1,],L.IPDE.ci[3,],col=adjustcolor("blue",0.8))
-ecoforecastR::ciEnvelope(time2,L.IP.ci[1,],L.IP.ci[3,],col=adjustcolor("green",0.8))
-ecoforecastR::ciEnvelope(time2,L.I.ci[1,],L.I.ci[3,],col=adjustcolor("gray",0.8))
-lines(time2,L.det,col="purple",lwd=2)
-
-
-###################Logistic with Covariates
-##Assemble Sf forecast
-GEFS_Directory <- paste("/projectnb/dietzelab/WeatherForecast/NOAA_GEFS/Data/",siteName,"/",forStartDate,"/",sep="")
-GEFS_Files <- dir(path=GEFS_Directory,pattern="NOAA_GEFS")
-
-TairsForecast <- numeric()
-for(e in 1:length(GEFS_Files)){
-  TairsForecastInd <- load_GEFS_Forecast(dataDirectory=GEFS_Directory,fileName=GEFS_Files[e])
-  TairsForecast <- cbind(TairsForecast,TairsForecastInd)
-}
-SfsALL <- matrix(nrow=0,ncol=length(forDates))
-
-for(e in 1:ncol(TairsForecast)){
-  Sfs <- calSf(Tairs=TairsForecast[,e],dates=Sf.dates)
-  SfsALL <- rbind(SfsALL,Sfs)
-}
-
-#save(SfsALL,file="SfsALL.RData")
-#load("SfsALL.RData")
-
-Sf.means <- matrix(colMeans(SfsALL),1,forecastLength)
-
-LCfileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_LC2_outBurn.RData",sep="")
-load(LCfileName)
-out.mat.par <- data.frame(as.matrix(outBurnLC2$params))
-dayNumber <- dim(as.matrix(outBurnLC2$predict))[2]-14
-out.mat.LC <- as.matrix(outBurnLC2$predict)
-out.mat.LC <- out.mat.LC[,1:dayNumber]
-ICs <- out.mat.LC[,dayNumber]
-
-##Deterministic prediction
-LC.det <- forecastLogCov(IC=mean(ICs),
-                     trans = mean(out.mat.par$trans),
-                     b1 = mean(out.mat.par$b1),
-                     Sf = Sf.means,
-                     Q=0,
-                     n=1,
-                     NT=14)
-
-##Initial Condition Uncertainty
-rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
-LC.I <- forecastLogCov(IC=ICs[rndNums],
-                   trans = mean(out.mat.par$trans),
-                   b1 = mean(out.mat.par$b1),
-                   Sf = Sf.means,
-                   Q=0,
-                   n=Nmc,
-                   NT=14)
-LC.I.ci <- apply(LC.I,2,quantile,c(0.025,0.5,0.975))
-
-##Parameter Uncertainty <- Don't have for RW (will have for log and logCov)
-LC.IP <- forecastLogCov(IC=ICs[rndNums],
-                    trans = out.mat.par$trans[rndNums],
-                    b1 = out.mat.par$b1[rndNums],
-                    Sf = Sf.means,
-                    Q=0,
-                    n=Nmc,
-                    NT=14)
-LC.IP.ci = apply(LC.IP,2,quantile,c(0.025,0.5,0.975))
-
-##Driver Uncertainty <- Don't have for RW (will have for logCov)
-drow = sample.int(nrow(SfsALL),Nmc,replace=TRUE)
-LC.IPD <- forecastLogCov(IC=ICs[rndNums],
-                        trans = out.mat.par$trans[rndNums],
-                        b1 = out.mat.par$b1[rndNums],
-                        Sf = SfsALL[drow,],
-                        Q=0,
+  RW.IC.ci <- apply(RW.IC,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Process Uncertainty
+  Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
+  RW.IPDE <- forecastRW(IC=ICs[rndNums],
+                        Q=Qmc,
                         n=Nmc,
                         NT=14)
-LC.IPD.ci = apply(LC.IPD,2,quantile,c(0.025,0.5,0.975))
-
-##Process Uncertainty
-Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
-LC.IPDE <- forecastLogCov(IC=ICs[rndNums],
-                       trans = out.mat.par$trans[rndNums],
-                       b1 = out.mat.par$b1[rndNums],
-                       Sf = SfsALL[drow,],
-                      Q=Qmc,
+  RW.IPDE.ci <- apply(RW.IPDE,2,quantile,c(0.025,0.5,0.975))
+  plotRun(out.mat = out.mat.RW,forecastType = "randomWalk",endDate=calEndDate)
+  ecoforecastR::ciEnvelope(time2,RW.IPDE.ci[1,],RW.IPDE.ci[3,],col=adjustcolor("blue",0.8))
+  ecoforecastR::ciEnvelope(time2,RW.IC.ci[1,],RW.IC.ci[3,],col=adjustcolor("gray",0.8))
+  lines(time2,RW.det,col="purple",lwd=2)
+  
+  #################Basic Logistic
+  logFileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_logistic_outBurn.RData",sep="")
+  load(logFileName)
+  out.mat.par <- data.frame(as.matrix(outBurnL$params))
+  dayNumber <- dim(as.matrix(outBurnL$predict))[2]-14
+  out.mat.L <- as.matrix(outBurnL$predict)
+  out.mat.L <- out.mat.L[,1:dayNumber]
+  ICs <- out.mat.L[,dayNumber]
+  
+  ##Deterministic prediction
+  L.det <- forecastLog(IC=mean(ICs),
+                       r=mean(out.mat.par$r),
+                       Q=0,
+                       n=1,
+                       NT=14)
+  
+  ##Initial Condition Uncertainty
+  rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
+  L.I <- forecastLog(IC=ICs[rndNums],
+                     r=mean(out.mat.par$r),
+                     Q=0,
+                     n=Nmc,
+                     NT=14)
+  L.I.ci <- apply(L.I,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Parameter Uncertainty <- Don't have for RW (will have for log and logCov)
+  L.IP <- forecastLog(IC=ICs[rndNums],
+                      r=out.mat.par$r[rndNums],
+                      Q=0,
                       n=Nmc,
                       NT=14)
-LC.IPDE.ci <- apply(LC.IPDE,2,quantile,c(0.025,0.5,0.975))
-plotRun(out.mat = out.mat.LC,forecastType = "logistic")
-ecoforecastR::ciEnvelope(time2,LC.IPDE.ci[1,],LC.IPDE.ci[3,],col=adjustcolor("blue",0.8))
-ecoforecastR::ciEnvelope(time2,LC.IPDE.ci[1,],LC.IPD.ci[3,],col=adjustcolor("pink",0.8))
-ecoforecastR::ciEnvelope(time2,LC.IP.ci[1,],LC.IP.ci[3,],col=adjustcolor("green",0.8))
-ecoforecastR::ciEnvelope(time2,LC.I.ci[1,],LC.I.ci[3,],col=adjustcolor("gray",0.8))
-lines(time2,LC.det,col="purple",lwd=2)
+  L.IP.ci = apply(L.IP,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Process Uncertainty
+  Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
+  L.IPDE <- forecastLog(IC=ICs[rndNums],
+                        r=out.mat.par$r[rndNums],
+                        Q=Qmc,
+                        n=Nmc,
+                        NT=14)
+  L.IPDE.ci <- apply(L.IPDE,2,quantile,c(0.025,0.5,0.975))
+  
+  plotRun(out.mat = out.mat.L,forecastType = "logistic",endDate=calEndDate)
+  ecoforecastR::ciEnvelope(time2,L.IPDE.ci[1,],L.IPDE.ci[3,],col=adjustcolor("blue",0.8))
+  ecoforecastR::ciEnvelope(time2,L.IP.ci[1,],L.IP.ci[3,],col=adjustcolor("green",0.8))
+  ecoforecastR::ciEnvelope(time2,L.I.ci[1,],L.I.ci[3,],col=adjustcolor("gray",0.8))
+  lines(time2,L.det,col="purple",lwd=2)
+  
+  
+  ###################Logistic with Covariates
+  ##Assemble Sf forecast
+  GEFS_Directory <- paste("/projectnb/dietzelab/WeatherForecast/NOAA_GEFS/Data/",siteName,"/",forStartDate,"/",sep="")
+  GEFS_Files <- dir(path=GEFS_Directory,pattern="NOAA_GEFS")
+  
+  TairsForecast <- numeric()
+  for(e in 1:length(GEFS_Files)){
+    TairsForecastInd <- load_GEFS_Forecast(dataDirectory=GEFS_Directory,fileName=GEFS_Files[e])
+    TairsForecast <- cbind(TairsForecast,TairsForecastInd)
+  }
+  SfsALL <- matrix(nrow=0,ncol=length(forDates))
+  
+  for(e in 1:ncol(TairsForecast)){
+    Sfs <- calSf(Tairs=TairsForecast[,e],dates=Sf.dates)
+    SfsALL <- rbind(SfsALL,Sfs)
+  }
+  
+  #save(SfsALL,file="SfsALL.RData")
+  #load("SfsALL.RData")
+  
+  Sf.means <- matrix(colMeans(SfsALL),1,forecastLength)
+  
+  LCfileName <- paste(forecastDataFolder,siteName,"_",siteStartDate,"_",calEndDate,"_LC2_outBurn.RData",sep="")
+  load(LCfileName)
+  out.mat.par <- data.frame(as.matrix(outBurnLC2$params))
+  dayNumber <- dim(as.matrix(outBurnLC2$predict))[2]-14
+  out.mat.LC <- as.matrix(outBurnLC2$predict)
+  out.mat.LC <- out.mat.LC[,1:dayNumber]
+  ICs <- out.mat.LC[,dayNumber]
+  
+  ##Deterministic prediction
+  LC.det <- forecastLogCov(IC=mean(ICs),
+                           trans = mean(out.mat.par$trans),
+                           b1 = mean(out.mat.par$b1),
+                           Sf = Sf.means,
+                           Q=0,
+                           n=1,
+                           NT=14)
+  
+  ##Initial Condition Uncertainty
+  rndNums <- sample.int(nrow(out.mat.par),Nmc,replace=TRUE)
+  LC.I <- forecastLogCov(IC=ICs[rndNums],
+                         trans = mean(out.mat.par$trans),
+                         b1 = mean(out.mat.par$b1),
+                         Sf = Sf.means,
+                         Q=0,
+                         n=Nmc,
+                         NT=14)
+  LC.I.ci <- apply(LC.I,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Parameter Uncertainty <- Don't have for RW (will have for log and logCov)
+  LC.IP <- forecastLogCov(IC=ICs[rndNums],
+                          trans = out.mat.par$trans[rndNums],
+                          b1 = out.mat.par$b1[rndNums],
+                          Sf = Sf.means,
+                          Q=0,
+                          n=Nmc,
+                          NT=14)
+  LC.IP.ci = apply(LC.IP,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Driver Uncertainty <- Don't have for RW (will have for logCov)
+  drow = sample.int(nrow(SfsALL),Nmc,replace=TRUE)
+  LC.IPD <- forecastLogCov(IC=ICs[rndNums],
+                           trans = out.mat.par$trans[rndNums],
+                           b1 = out.mat.par$b1[rndNums],
+                           Sf = SfsALL[drow,],
+                           Q=0,
+                           n=Nmc,
+                           NT=14)
+  LC.IPD.ci = apply(LC.IPD,2,quantile,c(0.025,0.5,0.975))
+  
+  ##Process Uncertainty
+  Qmc <- 1/sqrt(out.mat.par[rndNums,"p.proc"])
+  LC.IPDE <- forecastLogCov(IC=ICs[rndNums],
+                            trans = out.mat.par$trans[rndNums],
+                            b1 = out.mat.par$b1[rndNums],
+                            Sf = SfsALL[drow,],
+                            Q=Qmc,
+                            n=Nmc,
+                            NT=14)
+  LC.IPDE.ci <- apply(LC.IPDE,2,quantile,c(0.025,0.5,0.975))
+  plotRun(out.mat = out.mat.LC,forecastType = "logistic",endDate=calEndDate)
+  ecoforecastR::ciEnvelope(time2,LC.IPDE.ci[1,],LC.IPDE.ci[3,],col=adjustcolor("blue",0.8))
+  ecoforecastR::ciEnvelope(time2,LC.IPDE.ci[1,],LC.IPD.ci[3,],col=adjustcolor("pink",0.8))
+  ecoforecastR::ciEnvelope(time2,LC.IP.ci[1,],LC.IP.ci[3,],col=adjustcolor("green",0.8))
+  ecoforecastR::ciEnvelope(time2,LC.I.ci[1,],LC.I.ci[3,],col=adjustcolor("gray",0.8))
+  lines(time2,LC.det,col="purple",lwd=2)
+}
 
 dev.off()
