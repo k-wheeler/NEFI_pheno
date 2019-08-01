227ecf2e9e19f8d431edc0a940536866cdee934e
Kathryn Wheeler <kiwheel@bu.edu>
2019-08-01 16:39:16 -0400

    debugging

--- PhenoForecast/R/createSf.R
+++ PhenoForecast/R/createSf.R
@@ -2,33 +2,38 @@
 ##'
 ##' @param lat The desired latitude if not willowCreek
 ##' @param long The desired longitude if not willowCreek
-##' @param dates The desired dates
+##' @param dates The desired days
 ##' @param siteName The site name
 ##' @param dataDirectory The data directory
 ##' @param endDate The end date
 ##' @param GEFS_Files A vector of the file names for the GEFS files
 ##' @param GEFS_Directory The directory where the GEFS files are located
+##' @param calDatesT
 ##' @export
-createSf <- function(lat="",long="",dates,siteName,dataDirectory,endDate,GEFS_Files,GEFS_Directory,forecastLength, station) {
+createSf <- function(lat="",long="",dates,siteName,dataDirectory,endDate,GEFS_Files,GEFS_Directory,forecastLength, station,calDatesT=FALSE) {
   years <- lubridate::year(dates)
+  if(calDatesT){
+    calDates <- dates
+  }else{
   calDates <- seq(dates[1],as.Date(paste((years[length(years)]-1),"-12-31",sep="")),"day")
+  }
 
   ##The sources of the calibration and current measurements differ between willowCreek and other sites
-  if(siteName=="willowcreek"){
-    TairsCalInd <- download_US_WCr_met(start_date=calDates[1],end_date=calDates[length(calDates)])
-
-    ##Need to pad TairsCal to have ensembles
-    TairsCal <- matrix(ncol=10,nrow=length(TairsCalInd))
-    for(e in 1:10){
-      TairsCal[,e] <- TairsCalInd
-    }
-    TairsCurrentInd <- download_US_WCr_met(start_date=as.Date("2019-01-01"),end_date=(endDate-forecastLength))
-
-  }
-  else{
+  # if(siteName=="willowcreek"){
+  #   TairsCalInd <- download_US_WCr_met(start_date=calDates[1],end_date=calDates[length(calDates)])
+  #
+  #   ##Need to pad TairsCal to have ensembles
+  #   TairsCal <- matrix(ncol=10,nrow=length(TairsCalInd))
+  #   for(e in 1:10){
+  #     TairsCal[,e] <- TairsCalInd
+  #   }
+  #   TairsCurrentInd <- download_US_WCr_met(start_date=as.Date("2019-01-01"),end_date=(endDate-forecastLength))
+  #
+  # }
+  #else{
     TairsCal <- load_ERA5_Tair(lat=lat,long=long,years=seq(years[1],2018)) ##columns are each an ensemble (not divided by year)
     TairsCurrentInd <- load_NOAA_met(station=station,startDate=as.Date("2019-01-01"),endDate=(endDate-forecastLength)) ##Array of numeric values
-  }
+  #}
 
   ##GEFS Forecast (same for all sites) and pad TairsCurrent to be ensembles
   TairsCurrent <- matrix(ncol=length(GEFS_Files),nrow=length(TairsCurrentInd))
