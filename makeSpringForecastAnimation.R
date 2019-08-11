87e99257c9b56d723f3d6c00d6587d8cbb3637a7
Kathryn Wheeler <kiwheel@bu.edu>
2019-08-11 15:13:49 -0400

    debugging

--- makeSpringForecastAnimation.R
+++ makeSpringForecastAnimation.R
@@ -182,21 +182,21 @@ for(f in 1:length(forecastFiles)){
   time.p.date <- as.Date(phenoData$date[phenoData$year==2019])
   time.p <-  as.numeric(format(time.p.date,"%j"))
   
-  p <- rescale(c=cMeans.p[length(cMeans.p)],d=dMeans.p[length(dMeans.p)],yseq=p)
+  p <- PhenologyBayesModeling::rescale(c=cMeans.p[length(cMeans.p)],d=dMeans.p[length(dMeans.p)],yseq=p)
   p[p<0] <- 0
   
   ##MODIS NDVI and EVI
   mn <- prepareMODIS(startDate=startDate,endDate=endDate,metric="NDVI",timeForecast=time.p.date,dataDirectory=dataDirectory,siteName=siteName)
   me <- prepareMODIS(startDate=startDate,endDate=endDate,metric="EVI",timeForecast=time.p.date,dataDirectory=dataDirectory,siteName=siteName)
-  mn <- rescale(yseq=mn,c=cMeans.mn[length(cMeans.mn)],d=dMeans.mn[length(dMeans.mn)])
-  me <- rescale(yseq=me,c=cMeans.me[length(cMeans.me)],d=dMeans.me[length(dMeans.me)])
+  mn <- PhenologyBayesModeling::rescale(yseq=mn,c=cMeans.mn[length(cMeans.mn)],d=dMeans.mn[length(dMeans.mn)])
+  me <- PhenologyBayesModeling::rescale(yseq=me,c=cMeans.me[length(cMeans.me)],d=dMeans.me[length(dMeans.me)])
   mn[mn<0] <- 0
   me[me<0] <- 0
   load(paste(dataDirectory,"ForecastOutputs/AllForecasts/",forecastFiles[f],sep=""))
   
   if(mdlStr=="LC2"){
     outBurnLC <- outBurnLC2
-  }else if(mdlStr == "randomWalk"){
+  }else if(mdlStr == "RW"){
     outBurnLC <- outBurnRW
   }else if(mdlStr == "logistic"){
     outBurnLC <- outBurnL
@@ -206,7 +206,7 @@ for(f in 1:length(forecastFiles)){
   colnames(out.mat.par)
   
   ##Plot
-  par(mfrow=c(1,1))
+  
   dayNumber <- dim(as.matrix(outBurnLC$predict))[2]
   out.mat.LC <- as.matrix(outBurnLC$predict)
   
@@ -218,35 +218,34 @@ for(f in 1:length(forecastFiles)){
   png(file=plotFileName, width=10, height=5,units="in",res=1000)
   par(mfrow=c(1,1),mai=c(1,1,1,0.5))
   plotForecastOutput(siteName=siteName,URL=URL[length(URL)],forecastLength=forecastLength,
-                     out.mat=out.mat.lastYear,forecastType = "Random Walk",
-                     days=seq(1,lengthLastYear,1),xlim=c(10,160),
+                     out.mat=out.mat.lastYear,forecastType = "Logistic Covariate",
+                     days=seq(1,lengthLastYear,1),xlim=c(as.Date("2019-01-20"),as.Date("2019-06-10")),
                      plotTitle = "",endDate=endDate,
                      dates=plotDts)
-  abline(v=(lengthLastYear-forecastLength+1),col="purple",lwd=3)
+  abline(v=as.Date((lengthLastYear-forecastLength+1),origin=as.Date("2018-12-31")),col="purple",lwd=3)
   
   outFileName <- paste("PhenologyForecastData/phenoFits/",siteName,"_forecast_spring2019_varBurn.RData",sep="")
   
   ##First need 50% transition date estimate from the model:
   load(outFileName)
   var.mat <- data.frame(as.matrix(varBurn))
-  xseq <- seq(1,(181+365))
+  xseq <- seq(182,(181+365))
   ci <- createCI(PFT="DB",var.mat = var.mat,xseq=xseq,doRescale = TRUE,seasonOrder = "FS")
   ciDts <- as.Date(xseq,origin=as.Date("2017-12-31"))
-  ciEnvelope(x=ciDts,ylo =(ci[1,]), yhi = (ci[3,]),col=adjustcolor("gray",0.5))
+  ecoforecastR::ciEnvelope(x=ciDts,ylo=ci[1,],yhi = ci[3,],col=adjustcolor("gray",0.5))
   lines(ciDts,ci[2,],col=adjustcolor("gray",0.5))
-  #abline(v=(mean(var.mat$TranS)-365),col="cyan")
+  #abline(v=(mean(var.mat$TranS)-365),col="cyan",lwd=2)
   #abline(h=0.50,col="cyan")
   
   ##Add on data points
-  print(as.Date(time.p,origin=as.Date("2018-12-31")))
-  print(p)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),p,pch=20,col="red",cex=2)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),mn,col="blue",pch=3,cex=2)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),me,col="blue",pch=1,cex=2)
   #legend("topleft",c("PC","MODIS NDVI","MODIS EVI"),col=c("red","blue","blue"),pch=c(20,3,1))
   dev.off()
-  
 }
+
+
 mdlStr <- "logistic"
 
 siteName <- as.character(siteData$siteName[i])
@@ -299,21 +298,21 @@ for(f in 1:length(forecastFiles)){
   time.p.date <- as.Date(phenoData$date[phenoData$year==2019])
   time.p <-  as.numeric(format(time.p.date,"%j"))
   
-  p <- rescale(c=cMeans.p[length(cMeans.p)],d=dMeans.p[length(dMeans.p)],yseq=p)
+  p <- PhenologyBayesModeling::rescale(c=cMeans.p[length(cMeans.p)],d=dMeans.p[length(dMeans.p)],yseq=p)
   p[p<0] <- 0
   
   ##MODIS NDVI and EVI
   mn <- prepareMODIS(startDate=startDate,endDate=endDate,metric="NDVI",timeForecast=time.p.date,dataDirectory=dataDirectory,siteName=siteName)
   me <- prepareMODIS(startDate=startDate,endDate=endDate,metric="EVI",timeForecast=time.p.date,dataDirectory=dataDirectory,siteName=siteName)
-  mn <- rescale(yseq=mn,c=cMeans.mn[length(cMeans.mn)],d=dMeans.mn[length(dMeans.mn)])
-  me <- rescale(yseq=me,c=cMeans.me[length(cMeans.me)],d=dMeans.me[length(dMeans.me)])
+  mn <- PhenologyBayesModeling::rescale(yseq=mn,c=cMeans.mn[length(cMeans.mn)],d=dMeans.mn[length(dMeans.mn)])
+  me <- PhenologyBayesModeling::rescale(yseq=me,c=cMeans.me[length(cMeans.me)],d=dMeans.me[length(dMeans.me)])
   mn[mn<0] <- 0
   me[me<0] <- 0
   load(paste(dataDirectory,"ForecastOutputs/AllForecasts/",forecastFiles[f],sep=""))
   
   if(mdlStr=="LC2"){
     outBurnLC <- outBurnLC2
-  }else if(mdlStr == "randomWalk"){
+  }else if(mdlStr == "RW"){
     outBurnLC <- outBurnRW
   }else if(mdlStr == "logistic"){
     outBurnLC <- outBurnL
@@ -323,7 +322,7 @@ for(f in 1:length(forecastFiles)){
   colnames(out.mat.par)
   
   ##Plot
-  par(mfrow=c(1,1))
+  
   dayNumber <- dim(as.matrix(outBurnLC$predict))[2]
   out.mat.LC <- as.matrix(outBurnLC$predict)
   
@@ -336,34 +335,33 @@ for(f in 1:length(forecastFiles)){
   par(mfrow=c(1,1),mai=c(1,1,1,0.5))
   plotForecastOutput(siteName=siteName,URL=URL[length(URL)],forecastLength=forecastLength,
                      out.mat=out.mat.lastYear,forecastType = "Logistic Covariate",
-                     days=seq(1,lengthLastYear,1),xlim=c(10,160),
+                     days=seq(1,lengthLastYear,1),xlim=c(as.Date("2019-01-20"),as.Date("2019-06-10")),
                      plotTitle = "",endDate=endDate,
                      dates=plotDts)
-  abline(v=(lengthLastYear-forecastLength+1),col="purple",lwd=3)
+  abline(v=as.Date((lengthLastYear-forecastLength+1),origin=as.Date("2018-12-31")),col="purple",lwd=3)
   
   outFileName <- paste("PhenologyForecastData/phenoFits/",siteName,"_forecast_spring2019_varBurn.RData",sep="")
   
   ##First need 50% transition date estimate from the model:
   load(outFileName)
   var.mat <- data.frame(as.matrix(varBurn))
-  xseq <- seq(1,(181+365))
+  xseq <- seq(182,(181+365))
   ci <- createCI(PFT="DB",var.mat = var.mat,xseq=xseq,doRescale = TRUE,seasonOrder = "FS")
   ciDts <- as.Date(xseq,origin=as.Date("2017-12-31"))
-  ciEnvelope(x=ciDts,ylo =(ci[1,]), yhi = (ci[3,]),col=adjustcolor("gray",0.5))
+  ecoforecastR::ciEnvelope(x=ciDts,ylo=ci[1,],yhi = ci[3,],col=adjustcolor("gray",0.5))
   lines(ciDts,ci[2,],col=adjustcolor("gray",0.5))
-  #abline(v=(mean(var.mat$TranS)-365),col="cyan")
+  #abline(v=(mean(var.mat$TranS)-365),col="cyan",lwd=2)
   #abline(h=0.50,col="cyan")
   
   ##Add on data points
-  print(as.Date(time.p,origin=as.Date("2018-12-31")))
-  print(p)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),p,pch=20,col="red",cex=2)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),mn,col="blue",pch=3,cex=2)
   points(as.Date(time.p,origin=as.Date("2018-12-31")),me,col="blue",pch=1,cex=2)
   #legend("topleft",c("PC","MODIS NDVI","MODIS EVI"),col=c("red","blue","blue"),pch=c(20,3,1))
   dev.off()
-  
 }
+
+
 
 
 
