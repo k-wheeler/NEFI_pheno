5cbcbb9f7f5f434060e55cd34981203da362143d
Kathryn Wheeler <kiwheel@bu.edu>
2019-09-18 12:57:55 -0400

    debugging

--- mainPhenologyForecast_Autumn.R
+++ mainPhenologyForecast_Autumn.R
@@ -15,7 +15,7 @@ library(doParallel)
 
 ##Set and register cores for parallel
 n.cores <- 10
-registerDoParallel(cores=n.cores)
+#registerDoParallel(cores=n.cores)
 
 ##Read in data
 siteData <- read.csv("/projectnb/dietzelab/kiwheel/NEFI_pheno/PhenologyForecastData/phenologyForecastSites.csv",header=TRUE)
@@ -30,14 +30,14 @@ iseq <- c(15,16)
 iseq <- c(17,19)
 iseq <- c(9,19)
 iseq <- c(1)
-#iseq <- c(1,2,3,4,6,15,16,20,18,24)
+iseq <- c(1,2,3,4,6,15,16,20,18,24)
 
 endDate <- (Sys.Date()-1)
 print(endDate)
 #Create Forecast outputs
-output <- 
-foreach(i=iseq) %dopar% {
-#for(i in iseq){
+#output <- 
+#foreach(i=iseq) %dopar% {
+for(i in iseq){
   siteName <- as.character(siteData[i,1])
   print(siteName)
   GEFS_Directory <- paste("/projectnb/dietzelab/WeatherForecast/NOAA_GEFS/Data/",siteName,"/",endDate,"/",sep="")
@@ -59,7 +59,7 @@ foreach(i=iseq) %dopar% {
   ##Download MODIS data
   
   downloadMODIS(startDate=startDate,endDate=endDate,dataDirectory=dataDirectory,lat=lat,long=long,siteName=siteName)
-  
+  print("downloaded MODIS")
   ##Load rescaling data
   rescaleFile <- paste(dataDirectory,siteName,"_forecast_phenoFits_PC.csv",sep="")
   rescaleData <- read.csv(rescaleFile,header=TRUE)
@@ -80,7 +80,7 @@ foreach(i=iseq) %dopar% {
   ##Create Random Walk forecast if needed
   outputFile <- paste(saveDirectory,siteName,"_",startDate,"_",endDate,"_randomWalk_outBurn.RData",sep="")
   if(!file.exists(outputFile)){
-    outBurnRW <- phenologyForecast(forecastType = "randomWalk",forecastLength = forecastLength,
+    outBurnRW <- phenologyForecast_Autumn(forecastType = "randomWalk",forecastLength = forecastLength,
                                    siteName=siteName,URLs=URL,lat=lat,long=long,
                                    dataDirectory=dataDirectory,startDate,endDate,cValsPC=cMeans.p,
                                    dValsPC=dMeans.p,cValsMN=cMeans.mn,dValsMN=dMeans.mn,
@@ -93,7 +93,7 @@ foreach(i=iseq) %dopar% {
   #Create logistic forecast if needed
   outputFile <- paste(saveDirectory,siteName,"_",startDate,"_",endDate,"_logistic_outBurn.RData",sep="")
   if(!file.exists(outputFile)){
-    outBurnL <- phenologyForecast(forecastType = "logistic",forecastLength = forecastLength,
+    outBurnL <- phenologyForecast_Autumn(forecastType = "logistic",forecastLength = forecastLength,
                                   siteName=siteName,URLs=URL,lat=lat,long=long,dataDirectory=dataDirectory,
                                   startDate=startDate,endDate=endDate,cValsPC=cMeans.p,dValsPC=dMeans.p,
                                   cValsMN=cMeans.mn,dValsMN=dMeans.mn,cValsME=cMeans.me,dValsME=dMeans.me,
