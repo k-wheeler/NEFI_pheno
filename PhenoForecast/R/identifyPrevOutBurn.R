4efa52e038475f567f68214fb74aa12c7b24d1ac
Kathryn Wheeler <kiwheel@bu.edu>
2019-10-16 15:03:10 -0400

    debugging

--- PhenoForecast/R/identifyPrevOutBurn.R
+++ PhenoForecast/R/identifyPrevOutBurn.R
@@ -18,14 +18,13 @@ identifyPrevOutBurn <- function(siteName,forecastStr,startDate,endDate,dataDirec
     fileName <- paste(dataDirectory,"ForecastOutputs",siteName,"/",potentialDates[i],"/",
                       siteName,"_",startDate,"_",endDate,"_",forecastStr,
                       "_outBurn.RData",sep="")
-    #print(fileName)
+    print(fileName)
     if(file.exists(fileName)){
       fileFound <- TRUE
     }else if(i==1){
       fileFound <- TRUE
       fileName <- NA
-    }
-    else{
+    }else{
       i <- i - 1
     }
   }
