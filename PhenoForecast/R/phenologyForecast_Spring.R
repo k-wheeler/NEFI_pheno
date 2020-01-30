58391f8cc60b8cd6055780563f8938a7c00a9826
Kathryn Wheeler <kiwheel@bu.edu>
2020-01-30 16:09:05 -0500

    debugging

--- PhenoForecast/R/phenologyForecast_Spring.R
+++ PhenoForecast/R/phenologyForecast_Spring.R
@@ -18,22 +18,33 @@
 #' @param GEFS_Files The filenames for the GEFS files
 #' @param GEFS_Directory The directory where the GEFS files are located
 #' @param season The desired season: spring or fall
+#' @param baseTemp If CDD, the desired base temperature
+#' @param index The desired index (GCC, NDVI, EVI, or all)
+#' @param prevOutBurn The most recent previous forecast output
+#' @param baseNum
+#' @param iterSize
+#' @param effSize
+#' @param partialFile
 #' @import rjags
 #' @import runjags
 #' @import coda
 #' @import PhenologyBayesModeling
 #' @export
-phenologyForecast <- function(forecastType,forecastLength=14,siteName,
+phenologyForecast_Autumn <- function(forecastType,forecastLength=14,siteName,
                               URLs,lat,long,dataDirectory,startDate,endDate,
                               cValsPC,dValsPC,cValsMN,dValsMN,cValsME,dValsME,
-                              GEFS_Files="",GEFS_Directory,station="",season){
+                              GEFS_Files="",GEFS_Directory,station="",season,
+                              baseTemp=NA, index="all",
+                              prevOutBurn=NA,
+                              baseNum=10000,
+                              iterSize=5000,
+                              effSize=5000, partialFile=FALSE){
   print(forecastType)
   nchain=5
   ###Download PhenoCam data and format
-  #PCfileName <- paste(dataDirectory,siteName,"_",startDate,"_",endDate,"_PC_Data.RData",sep="")
-  #if(!file.exists(PCfileName)){
   phenoData <- matrix(nrow=0,ncol=32)
   for(u in 1:length(URLs)){
+    print(URLs[u])
     phenoDataSub <- download.phenocam(URLs[u])
     phenoData <- rbind(phenoData,phenoDataSub)
   }
@@ -44,78 +55,62 @@ phenologyForecast <- function(forecastType,forecastLength=14,siteName,
   phenoData <- phenoData[phenoData$date<endDate,]
   p.old <- phenoData$gcc_mean
   time.old <-  as.Date(phenoData$date)
-
   days <- seq(as.Date(startDate),(as.Date(endDate)+forecastLength),"day")
-  print(startDate)
-  print(endDate)
-
   p <- rep(NA,length(days))
 
   for(i in 1:length(p.old)){
     p[which(days==time.old[i])] <- p.old[i]
   }
-
   ##Download and format MODIS NDVI data
-  mn <- prepareMODIS(startDate=startDate,endDate=endDate,metric="NDVI",timeForecast=days,dataDirectory=dataDirectory,siteName=siteName)
-  me <- prepareMODIS(startDate=startDate,endDate=endDate,metric="EVI",timeForecast=days,dataDirectory=dataDirectory,siteName=siteName)
-
+  mn <- prepareMODIS(startDate=startDate,endDate=endDate,metric="NDVI",
+                     timeForecast=days,dataDirectory=dataDirectory,siteName=siteName)
+  me <- prepareMODIS(startDate=startDate,endDate=endDate,metric="EVI",
+                     timeForecast=days,dataDirectory=dataDirectory,siteName=siteName)
   months <- lubridate::month(days)
   years <- lubridate::year(days)
-  data <- list(x_ic=0,tau_ic = 1/(phenoData$g_std[1]**2))
+  data <- list()
 
   pdf(paste(siteName,"_",endDate,"_DataPlots.pdf",sep=""),width=10,height=8)
 
-  if(forecastType=="randomWalk"){
+  if(forecastType=="RW"){
     data$p <- rescaleObs(times=days,vals=p,partialStart=TRUE,cVals=cValsPC,dVals=dValsPC)
     data$n <- length(data$p)
     data$p[data$p<0] <- 0
     data$p[data$p>1] <- 1
-    data$mn <- rescaleObs(times=days,vals=mn,partialStart=TRUE,cVals=cValsMN,dVals=dValsMN)
-    data$mn[data$mn<0] <- 0
-    data$mn[data$mn>1] <- 1
-    data$me <- rescaleObs(times=days,vals=me,partialStart=TRUE,cVals=cValsME,dVals=dValsME)
-    data$me[data$me<0] <- 0
-    data$mn[data$mn>1] <- 1
-    print("days")
-    print(days)
-    print("data$p")
-    print(data$p)
+    #data$mn <- rescaleObs(times=days,vals=mn,partialStart=TRUE,cVals=cValsMN,dVals=dValsMN)
+    #data$mn[data$mn<0] <- 0
+    #data$mn[data$mn>1] <- 1
+    #data$me <- rescaleObs(times=days,vals=me,partialStart=TRUE,cVals=cValsME,dVals=dValsME)
+    #data$me[data$me<0] <- 0
+    #data$mn[data$mn>1] <- 1
     plot(days,data$p,pch=20,main="PhenoCam Data")
     abline(v=endDate,col="red")
     plot(days,data$mn,pch=20,main="MODIS NDVI Data")
     abline(v=endDate,col="red")
     plot(days,data$me,pch=20,main="MODIS EVI Data")
     abline(v=endDate,col="red")
-    print("Done with formatting data")
     dev.off()
 
-    j.model <- randomWalkPhenoModel(data=data,nchain=nchain)
+    j.model <- randomWalkPhenoModel(data=data,nchain=nchain,index = index)
     print("Done with creating the  random walk model")
     variableNames <- c("p.PC","p.MN","p.ME","p.proc","x")
     out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,baseNum = 5000,iterSize = 5000)
-  }else if(forecastType=="logistic" || forecastType== "logisticCov" || forecastType== "logisticCov2"|| forecastType== "logisticCov3"){
+  }else if(forecastType=="log" || forecastType== "CDD_meanTair"){
     dat2 <- data.frame(dates=days,years=years,months=months,p=p,mn=mn,me=me)
-    if(forecastType=="logisticCov" || forecastType== "logisticCov2"|| forecastType== "logisticCov3"){
-      datSf <- createSf(lat=lat,long=long,dates=days,siteName=siteName,dataDirectory=dataDirectory,endDate=(endDate+forecastLength),GEFS_Files=GEFS_Files,GEFS_Directory=GEFS_Directory,forecastLength=forecastLength,station=station)
-
-      dat2$Sf <- datSf$Sf
-      dat2$Sfprec <- datSf$Sfprec
+    if(forecastType=="CDD_meanTair"){
+      datTairs <- createTairs(lat=lat,long=long,dates=days,siteName=siteName,dataDirectory=dataDirectory,
+                              endDate=(endDate+forecastLength),GEFS_Files=GEFS_Files,
+                              GEFS_Directory=GEFS_Directory,forecastLength=forecastLength,station=station)
+      dat2$TairMu <- datTairs$TairMu
+      dat2$TairPrec<- datTairs$TairPrec
     }
-
-    #dat2 <- dat2[dat2$months%in%seq(1,6,1),]
-    if(season=="spring"){
-      dat2 <- dat2[as.numeric(format(dat2$dates,"%j"))%in% seq(1,181),]
-      nrowNum <- 181
-    }else if(season=="fall"){
-      dat2 <- dat2[as.numeric(format(dat2$dates,"%j"))%in% seq(182,365),]
-      nrowNum <- 184
-    }
-
+    dat2 <- dat2[as.numeric(format(dat2$dates,"%j"))%in% seq(213,365),]
+    nrowNum <- 365-213
     p <- matrix(nrow=nrowNum,ncol=0)
-    Sf <- matrix(nrow=nrowNum,ncol=0)
+    TairMu <- matrix(nrow=nrowNum,ncol=0)
     mn <- matrix(nrow=nrowNum,ncol=0)
     me <- matrix(nrow=nrowNum,ncol=0)
-    Sfprecs<- matrix(nrow=nrowNum,ncol=0)
+    TairPrec <- matrix(nrow=nrowNum,ncol=0)
     valNum <- 0
     days2 <- matrix(nrow=nrowNum,ncol=0)
     for(i in (lubridate::year(as.Date(dat2$dates[1]))):lubridate::year(as.Date(dat2$dates[length(dat2$dates)]))){##I know this includes the forecasted stuff, but it shouldn't really matter because of the JAGS model setup
@@ -126,15 +121,12 @@ phenologyForecast <- function(forecastType,forecastLength=14,siteName,
       }
       c <- cValsPC[valNum]
       d <- dValsPC[valNum]
-
       p <- cbind(p,rescale(yseq=subDat$p,c=c,d=d))
-      #print(subDat$dates)
       days2 <- cbind(days2,as.Date(subDat$dates))
-      if(forecastType=="logisticCov" || forecastType== "logisticCov2" || forecastType== "logisticCov3"){
-        Sf <- cbind(Sf,subDat$Sf)
-        Sfprecs <- cbind(Sfprecs,subDat$Sfprec)
+      if(forecastType=="CDD_meanTair"){
+        TairMu <- cbind(TairMu,subDat$TairMu)
+        TairPrec <- cbind(TairPrec,subDat$TairPrec)
       }
-
       c <- cValsMN[valNum]
       d <- dValsMN[valNum]
       mn <- cbind(mn,rescale(yseq=subDat$mn,c=c,d=d))
@@ -148,11 +140,15 @@ phenologyForecast <- function(forecastType,forecastLength=14,siteName,
     dataFinal$N <- ncol(dataFinal$p)
     dataFinal$x_ic <- 0
     dataFinal$tau_ic <- 1/(phenoData$g_std[1]**2)
-    if(season=="fall" && forecastType=="logistic"){
-      dataFinal$q <- as.numeric(format(endDate,"%j"))+forecastLength - 181
-    }else{
+    if(season=="fall" && forecastType=="log"){
+      dataFinal$q <- as.numeric(format(endDate,"%j"))+forecastLength - 213
+    }else if(season=="fall" && (forecastType=="CDD_meanTair")){
+      dataFinal$q <- as.numeric(format(endDate,"%j"))+forecastLength - 213
+    }
+    else{
       dataFinal$q <- as.numeric(format(endDate,"%j"))+forecastLength
     }
+
     plot(days2,dataFinal$p,pch=20,main="PhenoCam Data")
     abline(v=endDate,col="red")
     plot(days2,dataFinal$mn,pch=20,main="MODIS NDVI Data")
@@ -161,47 +157,53 @@ phenologyForecast <- function(forecastType,forecastLength=14,siteName,
     abline(v=endDate,col="red")
 
     print("Done with formating data")
-    if(forecastType=="logistic"){
+    if(forecastType=="log"){
       dev.off()
-      j.model <- logisticPhenoModel(data=dataFinal,nchain=nchain,season=season)
+      j.model <- logisticPhenoModel(data=dataFinal,nchain=nchain,season=season,index=index)
       print("Done creating the basic logistic model")
       variableNames <- c("p.proc","p.PC","p.ME","p.MN","x","r")
       out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,baseNum=10000,iterSize=5000)
-    }else if(forecastType=="logisticCov"){
-      dataFinal$Sfmu <- Sf
-      dataFinal$Sfprec <- Sfprecs
-      plot(days2,dataFinal$Sfmu,pch=20,main="Sf")
+    }else if(forecastType=="CDD_meanTair"){
+      dataFinal$TairMu <- TairMu
+      dataFinal$TairPrec <- TairPrec
+      ##Save Tair data:
+      TairData <- list(TairMu=dataFinal$TairMu,TairPrec=dataFinal$TairPrec)
+      TairFileName <- paste(siteName,"_",endDate,"_meanTair.R",sep="")
+      save(TairData,file=TairFileName)
+
+      plot(days2,dataFinal$TairMu,pch=20,main="Tair")
       abline(v=endDate,col="red")
       dev.off()
-      j.model <- logisticCovPhenoModel(data=dataFinal,nchain=nchain)
-      print("Done creating the logistic with covariate model")
-      variableNames <- c("p.PC","p.MN","p.ME","x","p.proc","b1","b0")
-      print(variableNames)
-      out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,baseNum=20000,iterSize=10000)
-    }else if(forecastType=="logisticCov2"){
-      dataFinal$Sfmu <- Sf
-      dataFinal$Sfprec <- Sfprecs
 
-      print("Creating logistic with covariate model 2")
-      j.model <- logisticCovPhenoModel2(data=dataFinal,nchain=nchain)
-      print("Done creating the logistic with covariate model 2")
-      variableNames <- c("p.PC","p.MN","p.ME","x","p.proc","b1","trans")
-      print(variableNames)
-      out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,baseNum=5000,iterSize=2000,effSize=1500)
-    }else if(forecastType=="logisticCov3"){
-      dataFinal$Sfmu <- Sf
-      dataFinal$Sfprec <- Sfprecs
+      print("Done creating the CDD covariate model")
 
-      print("Creating logistic with covariate model 3")
-      j.model <- logisticCovPhenoModel3(data=dataFinal,nchain=nchain)
-      print("Done creating the logistic with covariate model 3")
-      variableNames <- c("p.PC","p.MN","p.ME","x","p.proc","b1","trans")
+      if(is.na(baseTemp)){
+        variableNames <- c("baseTemp","p.proc","fallLength","x","MOF","sSlope")
+      }else{
+        variableNames <- c("p.proc","fallLength","x","MOF","sSlope")
+      }
+      if(index=="GCC"){
+        j.model <- phenoModel_CDD_Autumn_GCC(data=dataFinal,nchain=nchain,baseTemp=baseTemp)
+        variableNames <- c(variableNames, "p.PC")#,"x","p.proc","fallLength","MOF","sSlope","baseTemp")
+      }else if(index == "NDVI"){
+        j.model <- phenoModel_CDD_Autumn_NDVI(data=dataFinal,nchain=nchain,baseTemp=baseTemp)
+        variableNames <- c(variableNames,"p.MN")#,"x","p.proc","fallLength","MOF","sSlope","baseTemp")
+      }else if(index == "EVI"){
+        j.model <- phenoModel_CDD_Autumn_EVI(data=dataFinal,nchain=nchain,baseTemp=baseTemp)
+        variableNames <- c(variableNames,"p.ME")#,"x","p.proc","fallLength","MOF","sSlope","baseTemp")
+      }else{
+        variableNames <- c(variableNames,"p.PC","p.MN","p.ME")#,"x","p.proc","fallLength","MOF","sSlope","baseTemp")
+        j.model <- phenoModel_CDD_Autumn(data=dataFinal,nchain=nchain,
+                                         baseTemp=baseTemp)
+      }
+
       print(variableNames)
-      out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,baseNum=2000,iterSize=3000,effSize = 5000)
+      out.burn <- runForecastIter(j.model=j.model,variableNames=variableNames,
+                                  baseNum=baseNum,iterSize=iterSize,effSize=effSize,
+                                  partialFile=partialFile)
     }else{
       print("Forecast type not known!!!")
     }
-
   }
   print("Done with iterations")
 
@@ -215,6 +217,4 @@ phenologyForecast <- function(forecastType,forecastLength=14,siteName,
 
   print("Done thinning")
   return(out.burn)
-  #return(FALSE)
-
 }
